e46a680597431bf87de0270b8f076587822808e4
Jerry <873889292@qq.com>
2025-07-18 14:15:19 +0800

    Delete src directory

--- src/constants/constants.js
+++
@@ -1,9 +0,0 @@
-
-export const ROUTER_REPORT_KEY = "router";
-export const JS_ERROR_REPORT_KEY = "js_error";
-export const STATIC_ERROR_REPORT_KEY = "static_error";
-export const NETWORK_REPORT_KEY = "network";
-export const TRACK_REPORT_KEY = "custom";
-export const PERFORMANCE_REPORT_KEY = "performance";
-export const BLANK_SCREEN_REPORT_KEY = "blank_screen";
-
