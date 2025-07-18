afaf21e4f08f58d9ac1f2e7af599d8c7e5fa94c7
Jerry <873889292@qq.com>
2025-07-18 14:16:25 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
 
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import vue from "@vitejs/plugin-vue";
 
+// https://vite.dev/config/
 export default defineConfig({
+    server: {
+        cors: false,
+        open: true,
+        port: 5172,
+    },
+    optimizeDeps: {
+        include: ["web-vitals"], // 你使用了 web-vitals，可加入这里
+    },
     build: {
-        lib: {
-            entry: "src/index.js",
-            name: "MonitorSDK",
-            fileName: format => `monitor-sdk.${ format }.js`,
-            formats: ["es", "umd"]
-        },
         rollupOptions: {
             output: {
                 globals: {
                     axios: "axios"
                 }
             },
-            external: ["axios"] // 不打包 axios 等第三方依赖
+            external: ["axios", "vue"] // 不打包 axios 等第三方依赖
         }
     },
     plugins: [
-        dts(),
+        vue()
     ],
-});
+})
+
